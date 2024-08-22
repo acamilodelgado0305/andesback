043e58cb16dbcafe3d3656c121c27afefa08897e
Camilo Delgado <camilodelgado0305@gmail.com>
2024-08-22 12:48:36 -0500

    deploy

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 7001;
 app.use(cookieParser());
 app.use(cors({
     credentials: true,
-    origin: ['http://localhost:5173']
+    origin: ['http://localhost:5173','https://andesfront.onrender.com']
 }));
 app.use(express.json());
 
