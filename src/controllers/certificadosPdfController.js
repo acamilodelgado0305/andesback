34cf7718a30122c0c16b7c3de13ee19e28010633
Andres <TuEmail@example.com>
2025-12-10 15:51:43 -0500

    generar pdf

--- src/controllers/certificadosPdfController.js
+++ src/controllers/certificadosPdfController.js
@@ -336,7 +336,9 @@ export const generarCertificadoPDF = async (req, res) => {
         `;
 
         // --- PLAYWRIGHT GENERATION ---
-        browser = await chromium.launch({ headless: true });
+        browser = await chromium.launch({ headless: true ,
+            args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-dev-shm-usage']
+        });
         const page = await browser.newPage();
         await page.setContent(htmlContent);
 
